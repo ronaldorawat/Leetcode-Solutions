a9037ece2f4c4d2b32a696526b621b227e50477b
Ronaldo <31548338+ronaldorawat@users.noreply.github.com>
2024-01-19 23:38:37 +0530

    Time: 66 ms (51.62%) | Memory: 52.2 MB (5.35%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,16 +3,23 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let hash = new Map();
-    for(let i of nums) {
-        if(hash.has(i)) {
-            hash.delete(i);
-        }
-        else {
-            hash.set(i, i);
-        }
-    }
-    let res;
-    hash.forEach (function(value, key) { res = key});
-    return res;
+    // let hash = new Map();
+    // for(let i of nums) {
+    //     if(hash.has(i)) {
+    //         hash.delete(i);
+    //     }
+    //     else {
+    //         hash.set(i, i);
+    //     }
+    // }
+    // let res;
+    // hash.forEach (function(value, key) { res = key});
+    // return res;
+
+    let a= nums.sort((a,b)=>a-b);
+
+    for(let i=0;i<a.length;i+=2){
+        if(a[i]!==a[i+1])
+           return a[i];   
+}
 };
