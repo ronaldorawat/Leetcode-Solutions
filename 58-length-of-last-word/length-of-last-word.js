46df64968ee25517b67da83f99994823e34ad622
Ronaldo <31548338+ronaldorawat@users.noreply.github.com>
2024-02-06 12:13:08 +0530

    Time: 61 ms (16.97%) | Memory: 48.3 MB (26.24%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let trimmedString = s.trim();
+    
+    return trimmedString.length - trimmedString.lastIndexOf(' ') - 1;
+};
