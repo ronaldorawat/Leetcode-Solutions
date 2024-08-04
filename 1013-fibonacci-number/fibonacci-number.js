a99fbac7321ed4d3644a486a10f2c09633914f52
Ronaldo <31548338+ronaldorawat@users.noreply.github.com>
2024-08-05 01:01:28 +0530

    Time: 47 ms (87.20%) | Memory: 48.5 MB (78.60%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    const sqrt5 = Math.sqrt(5);
+    const fibN = Math.pow((1 + sqrt5) / 2, n) - Math.pow((1 - sqrt5) / 2, n);
+    return Math.round(fibN / sqrt5);
+};
