3d1488a02c93e13c21cc6137b3cf8c3f7f0e7bd7
Ronaldo <31548338+ronaldorawat@users.noreply.github.com>
2024-11-25 23:19:11 +0530

    Time: 1 ms (76.85%) | Memory: 49.2 MB (59.01%) - LeetSync

--- 392-is-subsequence/is-subsequence.js
+++ 392-is-subsequence/is-subsequence.js
@@ -1,20 +1,13 @@
-const isSubsequence = (s, t) => {
-  //! Edge case
-  if (s.length > t.length) return false; //! if len of s is greater than len of t, return false because s cant be a subsequence of t
-  `
-  Example:
-    s='Leetcode'
-    t='Code'
-    here we are trying to find if 'Leetcode' is a subsequence of 'Code' which is not possible because 'Leetcode' is longer than 'Code'
+var isSubsequence = function(s, t) {
+    let sp = 0;
+    let tp = 0;
 
-  `;
-  const t_length = t.length;
-  let subsequence = 0;
-  for (let i = 0; i < t_length; i++) {
-    if (s[subsequence] === t[i]) {
-      // ! if it is matching, increment subsequence
-      subsequence++;
+    while (sp < s.length && tp < t.length) {
+        if (s[sp] === t[tp]) {
+            sp++;
+        }
+        tp++;
     }
-  }
-  return subsequence === s.length
+
+    return sp === s.length;    
 };
