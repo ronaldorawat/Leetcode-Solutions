9f7d058bda3ad7407855e223998fab4b98ca7ef8
Ronaldo <31548338+ronaldorawat@users.noreply.github.com>
2024-12-08 19:42:31 +0530

    Time: 27 ms (100.00%) | Memory: 60.5 MB (70.97%) - LeetSync

---
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.js
@@ -0,0 +1,17 @@
+function minimumSize(n, maxOps) {
+    let low = 1;
+    let high = Math.max(...n);
+    while (low < high) {
+        const mid = (low + high) >> 1;
+        let cnt = 0;
+        for (const x of n) {
+            cnt += ~~((x - 1) / mid);
+        }
+        if (cnt <= maxOps) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return low;
+}
